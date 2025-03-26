3a76a2ff65af1564b0a1bec99dcb34f986af7cfe
Sarah-k984 <kiuriresarah@gmail.com>
2025-03-26 21:09:34 +0300

    my_list
    week 2 assignment python

---
+++ my-list.py
@@ -0,0 +1,17 @@
+#my-list
+my_list=[20,30,40]
+print(my_list)
+#list slicing
+print(my_list[0])
+print("before append:",my_list)
+numbers.Append(15)
+print("after append:",my_list)
+my_list=[20,30,40]
+print("list1:",my_list)
+Extending_numbers=[50,60,70]
+print("list2:",Extending_numbers)
+print("list after append:",my_list)
+my_list[20,30,40,50,60,70]
+del my_list[5]
+print my_list
+
